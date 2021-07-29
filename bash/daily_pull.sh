b8f33e2460976a4e840b1b7e8e5e092882f5bc8f
anthonyrinaldi4 <anthony.rinaldi04@gmail.com>
2021-07-29 11:11:06 -0400

    Add date logging to bash file

--- bash/daily_pull.sh
+++ bash/daily_pull.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # pull holdings from etfs
 # run daily
-python /home/pi/dev/etf_tracking/python_scripts/daily_pull.py
+now=$(date +'%d-%m-%Y')
+echo $now
+/usr/bin/python /home/pi/dev/etf_tracking/python_scripts/daily_pull.py
 echo 'DONE'
 read -t 60 -p 'Press any key to continue...'

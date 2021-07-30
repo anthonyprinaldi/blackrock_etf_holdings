46a9ad5b54e893c4a7e0b184496337b96499f130
anthonyrinaldi4 <anthony.rinaldi04@gmail.com>
2021-07-30 10:16:21 -0400

    Add sudo to bash

--- bash/daily_pull.sh
+++ bash/daily_pull.sh
@@ -3,6 +3,6 @@
 # run daily
 now=$(date +'%d-%m-%Y')
 echo $now
-/usr/bin/python /home/pi/dev/etf_tracking/python_scripts/daily_pull.py
+sudo /usr/bin/python /home/pi/dev/etf_tracking/python_scripts/daily_pull.py
 echo 'DONE'
 read -t 60 -p 'Press any key to continue...'
